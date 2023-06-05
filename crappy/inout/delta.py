9c677bb6beb728cc6d4be9971874584087f1fa61
SwallowedStar <etienne.malacarne@gmail.com>
2023-06-05 12:49:07 +0100

    add delta base

---
+++ crappy/inout/delta.py
@@ -0,0 +1,107 @@
+import crappy
+import time
+import pyvisa
+from .inout import InOut
+from datetime import datetime
+from .general_class import General
+from .general_class import LoggerPerso
+import logging
+from pynrfjprog import LowLevel
+import asyncio
+from bleak import BleakClient, BleakScanner
+class Delta(InOut,LoggerPerso):
+
+    def __init__(self):
+        try:
+            class_name=__name__
+            LoggerPerso.__init__(self,class_name)
+            self.logger.info(" Initialization of the device ...")
+            super().__init__()
+            self.api=None
+            self.bool_res=False
+
+            self.logger.info(" Initialization done !")
+        except Exception as e:
+            self.logger.info(" Error : initialization of the device is incorrect ")
+
+
+    def open(self):
+        pass
+     
+    def connexion_bluetooth(self,id):
+        self.logger.info("Begin testing bluetooth connexion ...")
+     
+        async def run():
+            scanner = BleakScanner()
+            devices = await scanner.discover()
+
+            for device in devices:
+                pass
+
+            async with BleakClient(id) as client:
+                print(f"Connected: {client.is_connected}")
+                self.bool_res=client.is_connected
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(run())
+        res=self.bool_res
+        self.bool_res=False
+        self.logger.info(self.bool_res)
+        return self.bool_res
+
+
+
+
+    def flash_firmware(self):
+        bool_res=False
+        with LowLevel.API('NRF52') as api:
+            self.logger.info("Open connection with JLINK ...")
+            try:
+                api.open()
+                api.connect_to_emu_with_snr(821005840)
+                self.logger.info("Connection done !")
+            except Exception as e:
+                pass
+            
+            self.logger.info("Open connection with device ...")
+            try:
+                api.connect_to_device()
+                self.logger.info("Connection done !")
+            except Exception as e:
+                pass
+
+    
+            try:
+                self.logger.info("Begin reset and flashing...")
+
+                api.sys_reset()
+                api.erase_all()
+                self.logger.info("Reset done !")
+
+                api.program_file("full.hex")
+
+                        
+                self.logger.info("Flashing done!")
+
+                api.sys_reset()
+                api.go()
+                self.logger.info("reset after flash done !")
+
+                api.close()
+                self.logger.info("Everything done! Device is ready to be used")
+
+                bool_res=True
+            except Exception as e:
+                pass
+        return bool_res
+            
+
+
+    def get_data(self):
+        return [time.time(), 0]
+
+    def set_cmd(self, cmd):
+        pass
+
+    def close(self):
+        pass
