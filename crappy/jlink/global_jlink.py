d8a13b6ad35ce135832a5a17871d5d94a64aae3e
SwallowedStar <etienne.malacarne@gmail.com>
2023-06-20 15:05:34 +0200

    update but no refac/opti

--- crappy/jlink/global_jlink.py
+++ crappy/jlink/global_jlink.py
@@ -1,39 +1,84 @@
 
 
-
+from .jlink import JLINK
 
 import pylink
 
 
-class Global_JLINK():
+class Global_JLINK(JLINK):
 
-    def __init__(self):
-        self.jlink = pylink.JLink()
+    def start_rtt(self):
+        if self.connected:
+            self.api.rtt_start()
+            self.rtt = True
+        else:
+            raise RuntimeError("NRF device is not connected.")
+    def get_data(self):
+        if self.connected:
+            data = None
+            return data
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-    def connect(self):
-        # Établir la connexion avec le J-Link
-        self.jlink.open()
+    def stop_rtt(self):
+        if self.connected and self.rtt:
+            self.api.rtt_stop()
+            self.rtt = False
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-        # Initialiser la cible (à adapter en fonction de votre configuration)
-        self.jlink.connect("Cortex-M4")
+    def read_rtt(self, up_channel_index=0, length=1024):
+        if self.connected and self.rtt:
+            return self.api.rtt_read(up_channel_index, length)
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-        # Configurer le mode de transfert en temps réel (RTT)
-        self.jlink.set_tif(pylink.enums.JLinkInterfaces.SWD)
+    def write_rtt(self, down_channel_index=0, data=None):
+        if self.connected and self.rtt and data is not None:
+            return self.api.rtt_write(down_channel_index, data)
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-        # Ouvrir le canal RTT
-        self.jlink.rtt_start()
+    def start_stream(self):
+        if self.connected:
+            pass
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-    def rtt_write(self, channel, data):
-        # Écrire des données RTT dans le canal spécifié
-        self.jlink.rtt_write(channel, data)
+    def get_stream(self):
+        if self.connected:
+            stream_data = None
+            return stream_data
+        else:
+            raise RuntimeError("NRF device is not connected.")
 
-    def rtt_read(self, channel, size):
-        # Lire des données RTT à partir du canal spécifié
-        return self.jlink.rtt_read(channel, size)
-
-    def disconnect(self):
-        # Arrêter le canal RTT
-        self.jlink.rtt_stop()
-
-        # Fermer la connexion avec le J-Link
-        self.jlink.close()
+    def stop_stream(self):
+        if self.connected:
+            pass
+        else:
+            raise RuntimeError("NRF device is not connected.")
+        
+    def read_data(self, addr, data_len):
+        if self.connected:
+            return self.api.read(addr, data_len)
+        else:
+            raise RuntimeError("NRF device is not connected.")
+    
+    def flash_firmware(self, firmware_path):
+        if self.connected:
+            try:
+                self.logger.info("Beginning reset and flashing...")
+                self.api.sys_reset()
+                self.api.erase_all()
+                self.logger.info("Reset done !")
+                self.api.program_file(firmware_path)
+                self.logger.info("Flashing done!")
+                self.api.sys_reset()
+                self.api.go()
+                self.logger.info("Reset after flash done!")
+                return True
+            except Exception as e:
+                self.logger.error("Error during flashing: %s", e)
+                return False
+        else:
+            raise RuntimeError("NRF device is not connected.")
--- crappy/jlink/jlink.py
+++ crappy/jlink/jlink.py
@@ -52,91 +52,11 @@ class JLINK(metaclass=MetaJLINK):
         self.api.close()
         self.connected = False
 
-    def get_data(self):
-        if self.connected:
-            data = None
-            # Code to get the data from the NRF device
-            return data
-        else:
-            raise RuntimeError("NRF device is not connected.")
 
     def set_cmd(self, *cmd):
         if self.connected:
-            # Code to handle commands
             pass
         else:
             raise RuntimeError("NRF device is not connected.")
 
-    def start_rtt(self):
-        if self.connected:
-            self.api.rtt_start()
-            self.rtt = True
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def stop_rtt(self):
-        if self.connected and self.rtt:
-            self.api.rtt_stop()
-            self.rtt = False
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def read_rtt(self, up_channel_index=0, length=1024):
-        if self.connected and self.rtt:
-            return self.api.rtt_read(up_channel_index, length)
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def write_rtt(self, down_channel_index=0, data=None):
-        if self.connected and self.rtt and data is not None:
-            return self.api.rtt_write(down_channel_index, data)
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def start_stream(self):
-        if self.connected:
-            # Code to start stream acquisition
-            pass
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def get_stream(self):
-        if self.connected:
-            stream_data=None
-            # Code to get stream data from the NRF device
-            return stream_data
-        else:
-            raise RuntimeError("NRF device is not connected.")
-
-    def stop_stream(self):
-        if self.connected:
-            # Code to stop stream acquisition
-            pass
-        else:
-            raise RuntimeError("NRF device is not connected.")
-        
-    
-    def read_data(self, addr, data_len):
-        if self.connected:
-            return self.api.read(addr, data_len)
-        else:
-            raise RuntimeError("NRF device is not connected.")
-    
-    def flash_firmware(self, firmware_path):
-        if self.connected:
-            try:
-                self.logger.info("Beginning reset and flashing...")
-                self.api.sys_reset()
-                self.api.erase_all()
-                self.logger.info("Reset done !")
-                self.api.program_file(firmware_path)
-                self.logger.info("Flashing done!")
-                self.api.sys_reset()
-                self.api.go()
-                self.logger.info("Reset after flash done!")
-                return True
-            except Exception as e:
-                self.logger.error("Error during flashing: %s", e)
-                return False
-        else:
-            raise RuntimeError("NRF device is not connected.")
+   
