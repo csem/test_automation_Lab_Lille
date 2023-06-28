f2572553e3e1f51c2270032e650363ece8663300
SwallowedStar <etienne.malacarne@gmail.com>
2023-06-28 14:50:48 +0200

    gest cas ssh

--- crappy/macro/__init__.py
+++ crappy/macro/__init__.py
@@ -1,3 +1,9 @@
 
-from .macro import MACRO
-from .pc_macro import PC_MACRO
+import os
+
+if 'DISPLAY' in os.environ:
+    from .macro import MACRO
+    from .pc_macro import PC_MACRO
+else:
+    pass
+
