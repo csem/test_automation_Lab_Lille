989584f98eab0a23831fde7bd0bf31d544990a4f
SwallowedStar <etienne.malacarne@gmail.com>
2023-07-20 15:03:49 +0200

    add basic gpio

--- crappy/gpio/global_gpio.py
+++ crappy/gpio/global_gpio.py
@@ -1,1 +1,31 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio_module
+from .gpio import GPIO
+
+class GlobalGPIO(GPIO):
+
+    def __init__(self, pin):
+        self.pin = pin
+        gpio_module.setmode(gpio_module.BCM)
+        gpio_module.setup(self.pin, gpio_module.OUT)
+
+    def turn_on(self):
+        gpio_module.output(self.pin, gpio_module.HIGH)
+
+    def turn_off(self):
+        gpio_module.output(self.pin, gpio_module.LOW)
+
+    def toggle(self):
+        if gpio_module.input(self.pin):
+            self.turn_off()
+        else:
+            self.turn_on()
+
+    def blink(self, duration=1, frequency=1):
+        interval = 1.0 / (2 * frequency) 
+        end_time = time.time() + duration
+        while time.time() < end_time:
+            self.toggle()
+            time.sleep(interval)
+
+    def cleanup(self):
+        gpio_module.cleanup(self.pin)
